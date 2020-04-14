4202c74eabe707ad326b6c172518c69b62d33f0c
Vinicius Rocha <viniciusrocha@mangobits.com>
2020-04-14 19:40:28 -0300

    initial commit;

---
+++ Tinder/Controller/CombineViewController.swift
@@ -0,0 +1,132 @@
+//
+//  CombineViewController.swift
+//  Tinder
+//
+//  Created by Vinicius Rocha on 08/03/20.
+//  Copyright © 2020 ViniciusRocha. All rights reserved.
+//
+
+import UIKit
+
+class CombineViewController: UIViewController {
+    
+    var perfilButton: UIButton = .iconMenu(named: "icone-perfil")
+    var chatButton: UIButton = .iconMenu(named: "icone-chat")
+    var logoButton: UIButton = .iconMenu(named: "icone-logo")
+    
+    var deslikeButton: UIButton = .iconFooter(named: "icone-deslike")
+    var likeButton: UIButton = .iconFooter(named: "icone-like")
+    var superlikeButton: UIButton = .iconFooter(named: "icone-superlike")
+    
+    var usuarios: [Usuario] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.isHidden = true
+        view.backgroundColor = UIColor.systemGroupedBackground
+        
+        self.adicionarFooter()
+        self.adicionaHeader()
+        self.buscaUsuarios()
+    }
+    
+    func buscaUsuarios(){
+        self.usuarios = UsuarioService.shared.buscaUsuarios()
+        self.adiconarCards()
+        
+    }
+}
+
+
+extension CombineViewController{
+    
+    func adiconarCards(){
+        
+        for usuario in usuarios {
+
+            let card = CombineCardView()
+            card.frame = CGRect(x: 0, y: 0, width: view.bounds.width - 32, height: view.bounds.height * 0.7)
+            card.center = view.center
+            card.usuario = usuario
+            card.tag = usuario.id
+            
+            let gesture = UIPanGestureRecognizer()
+            gesture.addTarget(self, action: #selector(handlerCard))
+            
+            card.addGestureRecognizer(gesture)
+            
+            view.insertSubview(card, at: 0)
+                
+        }
+    }
+}
+
+extension CombineViewController {
+    
+    func adicionaHeader() {
+        
+        let window = UIApplication.shared.windows.first { $0.isKeyWindow }
+        let top: CGFloat = window?.safeAreaInsets.top ?? 44
+        
+        let stackView = UIStackView(arrangedSubviews: [perfilButton, logoButton, chatButton])
+        stackView.distribution = .equalCentering
+        
+        view.addSubview(stackView)
+        stackView.preencher(
+            top: view.topAnchor,
+            leading: view.leadingAnchor,
+            trailing: view.trailingAnchor,
+            bottom: nil,
+            padding: .init(top: top, left: 16, bottom: 0, right: 16)
+        )
+    }
+    
+    func adicionarFooter() {
+        let stackView = UIStackView(arrangedSubviews: [ UIView(), deslikeButton, superlikeButton, likeButton, UIView() ])
+        stackView.distribution = .equalCentering
+        
+        view.addSubview(stackView)
+        stackView.preencher(
+            top: nil,
+            leading: view.leadingAnchor,
+            trailing: view.trailingAnchor,
+            bottom: view.bottomAnchor,
+            padding: .init(top: 0, left: 16, bottom: 34, right: 16))
+    }
+}
+
+
+extension CombineViewController {
+    
+    @objc func handlerCard (_ gesture: UIPanGestureRecognizer ) {
+        if let card = gesture.view as? CombineCardView {
+            let point = gesture.translation(in: view)
+            
+            card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+            
+            let rotationAngle = point.x / view.bounds.width * 0.4
+            
+            if point.x > 0 {
+                card.likeImageView.alpha = rotationAngle * 5
+                card.deslikeImageView.alpha = 0
+            } else {
+                card.likeImageView.alpha = 0
+                card.deslikeImageView.alpha = rotationAngle * 5 * -1
+            }
+            
+            card.transform = CGAffineTransform(rotationAngle: rotationAngle)
+            
+            if gesture.state == .ended {
+                
+                UIView.animate(withDuration: 0.2) {
+                    card.center = self.view.center
+                    card.transform = .identity
+                    
+                    card.likeImageView.alpha = 0
+                    card.deslikeImageView.alpha = 0
+                }
+            }
+        }
+    }
+}
---
+++ Tinder/Model/Usuario.swift
@@ -0,0 +1,18 @@
+//
+//  Usuario.swift
+//  Tinder
+//
+//  Created by Vinicius Rocha on 07/04/20.
+//  Copyright © 2020 ViniciusRocha. All rights reserved.
+//
+
+import Foundation
+
+struct Usuario {
+    let id: Int
+    let nome: String
+    let idade: Int
+    let match: Bool
+    let frase: String
+    let foto: String
+}
